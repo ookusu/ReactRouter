78e0534b2fc257ae6c685c6b4698f22cdac91763
ookusu <85726903+ookusu@users.noreply.github.com>
2021-10-29 12:44:52 +0900

    sintyoku2

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
+
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1 Page</h1>
+      <Link to="/Page1DetailA">DetailA</Link>
+      <Link to="/Page1DetailB">DetailB</Link>
     </div>
   );
 };
